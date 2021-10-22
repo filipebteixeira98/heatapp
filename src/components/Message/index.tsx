37d0e3341de3f9a283995f19b55ccaa07d143d0b
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-10-22 18:22:46 -0300

    chore: configure messages animation with moti

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { MotiView } from 'moti';
 
 import { styles } from './styles';
 
@@ -20,12 +21,17 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
       <View style={styles.footer}>
         <UserPhoto imageUri={data.user.avatar_url} sizes="SMALL" />
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
