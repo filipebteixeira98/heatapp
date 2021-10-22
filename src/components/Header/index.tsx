59d6e1c7fd5f190b3101e9fe79b0d366e2f8bf93
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-10-22 15:59:47 -0300

    chore: created Header & UserPhoto components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,18 @@ import { styles } from './styles';
 
 import LogoSvg from '../../assets/logo.svg';
 
+import { UserPhoto } from '../UserPhoto';
+
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sign out</Text>
-      </TouchableOpacity>
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sign out</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri="https://github.com/filipebteixeira98.png" />
+      </View>
     </View>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -10,9 +10,14 @@ export const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 20,
   },
+  logoutButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   logoutText: {
     fontSize: 15,
     fontFamily: FONTS.REGULAR,
     color: COLORS.WHITE,
+    marginRight: 20,
   },
 });
